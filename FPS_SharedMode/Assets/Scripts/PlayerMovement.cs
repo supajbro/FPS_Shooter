4b57e88a54707cb3793be1b48862eebdae5a6de6
supajbro <supajbro@gmail.com>
2025-01-12 17:11:37 +1000

    Implemented Jumping logic
    Jumping behaviour implemented and synced between clients

--- FPS_SharedMode/Assets/Scripts/PlayerMovement.cs
+++ FPS_SharedMode/Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,24 @@ public class PlayerMovement : NetworkBehaviour
     private CharacterController m_controller;
     [SerializeField] private Transform m_camPos;
     [SerializeField] private PlayerWeapon m_weapon;
+
     private Camera m_camera;
     public Camera Cam { get { return m_camera; } }
-    [SerializeField] private GameObject m_weaponHolder;
-    public NetworkObject WeaponHolder;
 
     private Vector3 m_velocity;
     private bool m_jumpPressed;
+    private bool m_startedJump;
+    [SerializeField] private LayerMask m_groundLayer;
 
     [Header("Values")]
     [SerializeField] private float m_speed = 2f;
-    [SerializeField] private float m_jumpForce = 5f;
     [SerializeField] private float m_gravity = -9.81f;
+
+    [Header("Jump Values")]
+    [SerializeField] private float m_jumpForce = 5f;
+    [SerializeField] private float m_maxJumpForce = 5f;
+
+    public bool isGrounded => IsGrounded();
 
     private void Awake()
     {
@@ -49,11 +55,6 @@ public class PlayerMovement : NetworkBehaviour
     {
         // FixedUpdateNetwork is only executed on the StateAuthority
 
-        if (!m_controller.isGrounded)
-        {
-            m_velocity = new Vector3(0, -1, 0);
-        }
-
         Quaternion camRotY = Quaternion.Euler(0, m_camera.transform.rotation.eulerAngles.y, 0);
         Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 move = camRotY * moveInput * Runner.DeltaTime * m_speed;
@@ -64,15 +65,43 @@ public class PlayerMovement : NetworkBehaviour
         Quaternion camRotX = Quaternion.Euler(m_camera.transform.rotation.eulerAngles.x, m_camera.transform.rotation.eulerAngles.y, 0);
         m_weapon.transform.rotation = camRotX;
 
-        m_velocity.y += m_gravity * Runner.DeltaTime;
-        if (m_jumpPressed && m_controller.isGrounded)
+        // Initialise the jump
+        if (m_jumpPressed && IsGrounded())
         {
-            m_velocity.y += m_jumpForce;
+            m_jumpPressed = false;
+            m_jumpForce = m_maxJumpForce;
+        }
+
+        // Update the jump force when the player is off the ground
+        if (!IsGrounded())
+        {
+            m_jumpForce -= Runner.DeltaTime * 7.5f;
+            m_jumpForce = Mathf.Clamp(m_jumpForce, -m_maxJumpForce, m_maxJumpForce);
+        }
+
+        // Update the y velocity and reset it to 0 if player is grounded
+        m_velocity.y += m_jumpForce;
+        if(IsGrounded() && m_jumpForce != m_maxJumpForce)
+        {
+            m_velocity.y = 0f;
         }
 
         m_controller.Move(move + m_velocity * Runner.DeltaTime);
+    }
 
-        m_jumpPressed = false;
+    private bool IsGrounded()
+    {
+        const float groundCheckDistance = 1.5f;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, groundCheckDistance, m_groundLayer))
+        {
+            Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.blue);
+            return true;
+        }
+
+        Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.red);
+        return false;
     }
 
 }
